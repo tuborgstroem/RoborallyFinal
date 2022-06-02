c392021b5aa4b67ed4936c3b453356d5aecfdec1
kasperkhansen <52578478+kasperkhansen@users.noreply.github.com>
2022-06-02 17:50:39 +0200

    Changes by Magnu

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/fieldActions/Checkpoint.java
@@ -0,0 +1,21 @@
+package dk.dtu.compute.se.pisd.roborally.model.fieldActions;
+
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import org.jetbrains.annotations.NotNull;
+
+public class Checkpoint extends FieldAction{
+    private int checkPointNo;
+
+
+    @Override
+    public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
+        // TODO needs to be implemented
+        return false;
+    }
+
+
+
+
+
+}
